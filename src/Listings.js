1ac4212ab8d078b9acc0da3f9ac131d7f4b50aee
Christopher Tokar <christopher.tokar@gmail.com>
2019-01-12 15:11:02 +0100

    cards layout fix

--- src/Listings.js
+++ src/Listings.js
@@ -2,22 +2,33 @@ import React from 'react';
 
 import styles from './Listing.module.css'; // Import css modules stylesheet as styles
 
+const divStyle = {
+  width: '33%',
+  float: 'left'
+};
+
+const imgStyle = {
+  height: '200px',
+  objectFit: 'cover',
+};
 
 const Listings = (props) => (
   <ul>
     {props.data.map(function (name, index) {
       const fullImg = '/img/' + name.img;
-      return <div className={styles.box}>
-            <div key={index}><img className={styles.thumb} src={fullImg} /></div>
-            <div key={index}>${name.price}</div>
-            <div key={index}>{name.address}</div>
-            <div key={index}>{name.bedrooms} bedrooms</div>
-            <div key={index}>{name.bathrooms} bathrooms</div>
-        </div>;
+      return <div className="card" style={divStyle}>
+        <div key={index}><img className="card-img-top" style={imgStyle} src={fullImg} /></div>
+        <div className="card-body">
+          <h5 className="card-title">${name.price}</h5>
+          <p className="card-text">{name.address}</p>
+          <p className="card-text">{name.bedrooms} bedrooms</p>
+          <p className="card-text">{name.bathrooms} bathrooms</p>
+        </div>
+      </div>;
     })}
-    
+
   </ul>
-  
+
 );
 
 export default Listings;
