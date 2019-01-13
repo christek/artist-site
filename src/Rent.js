9a1e14a82116fe899a35e34173baf53bc116395b
Christopher Tokar <christopher.tokar@gmail.com>
2019-01-13 17:14:08 +0100

    json server added and working

--- src/Rent.js
+++ src/Rent.js
@@ -3,40 +3,57 @@ import React from 'react';
 import Listings from './Listings';
 import Map from './Map';
 
-const data = [{
-  id: 'abc',
-  price: '300,000',
-  address: '11 hyde rd',
-  img: 'a.jpg',
-  bedrooms: 3,
-  bathrooms: 4,
-},
-{
-  id: 'def',
-  price: '500,000',
-  address: '12 hyde rd',
-  img: 'b.jpg',
-  bedrooms: 3,
-  bathrooms: 4,
-},
-{
-  id: 'ghi',
-  price: '400,000',
-  address: '13 hyde rd',
-  img: 'c.jpg',
-  bedrooms: 3,
-  bathrooms: 4,
-}];
+class Rent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
 
-const Rent = (props) => (
-  <div className="row">   
-      <div className="col-6">
-        <Listings data={data} />
-      </div>
-      <div className="col-6">
-        <Map />
-      </div>
-  </div>
-);
+  componentDidMount() {
+    fetch("http://localhost:3004/posts/")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div className="row">   
+        <div className="col-6">
+          <Listings data={items} />
+        </div>
+        <div className="col-6">
+          <Map />
+        </div>
+    </div>
+      );
+    }
+  }
+}
 
 export default Rent;
