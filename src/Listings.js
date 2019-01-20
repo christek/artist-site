cded6a07b35e68d6939e8c5f33bd1942b07f7340
Christopher Tokar <christopher.tokar@gmail.com>
2019-01-20 14:41:55 +0100

    made pagination work

--- src/Listings.js
+++ src/Listings.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Listing.css';
 import { BrowserRouter as Route, Link } from "react-router-dom";
 
 const cardStyle = {
@@ -12,15 +13,37 @@ const imgStyle = {
   objectFit: 'cover',
 };
 
-const Listings = (props) => (
-  <ul>
-    {props.data.map(function (name, index) {
+// https://codepen.io/PiotrBerebecki/pen/pEYPbY
+
+class Listings extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: props.data,
+      currentPage: 1,
+      todosPerPage: 2
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    this.setState({
+      currentPage: Number(event.target.id)
+    });
+  }
+
+  render() {
+    const { todos, currentPage, todosPerPage } = this.state;
+
+    // Logic for displaying current todos
+    const indexOfLastTodo = currentPage * todosPerPage;
+    const indexOfFirstTodo = indexOfLastTodo - todosPerPage;
+    const currentTodos = todos.slice(indexOfFirstTodo, indexOfLastTodo);
+
+    const renderTodos = currentTodos.map(function (name, index) {
       const fullImg = '/img/' + name.img;
       return <div className="card" key={index} style={cardStyle}>
-
         <Link to={`/properties/${name.id}`}>go to this</Link>
-    
-
         <div key={index}><img className="card-img-top" style={imgStyle} src={fullImg} alt='property' /></div>
         <div className="card-body">
           <h5 className="card-title">${name.price}</h5>
@@ -28,11 +51,38 @@ const Listings = (props) => (
           <p className="card-text">{name.bedrooms} bedrooms</p>
           <p className="card-text">{name.bathrooms} bathrooms</p>
         </div>
-      </div>;
-    })}
+      </div>
+    });
 
-  </ul>
+    // Logic for displaying page numbers
+    const pageNumbers = [];
+    for (let i = 1; i <= Math.ceil(todos.length / todosPerPage); i++) {
+      pageNumbers.push(i);
+    }
 
-);
+    const renderPageNumbers = pageNumbers.map(number => {
+      return (
+        <li
+          key={number}
+          id={number}
+          onClick={this.handleClick}
+        >
+          {number}
+        </li>
+      );
+    });
+
+    return (
+      <div>
+        <ul>
+      {renderTodos}
+        </ul>
+        <ul class="page-numbers">
+         {renderPageNumbers}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default Listings;
